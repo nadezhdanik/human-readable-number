11aea79fe232084d20e075d715010ce4e061e892
nadezhdanik <nadezhdanik@gmail.com>
2023-07-28 13:44:10 +0300

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,54 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    const ones = [
+        "zero",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+    ];
+    const tens = [
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
+    let backTwoDigits = +number.toString().slice(-2);
+    if (number < 20) {
+        return ones[number];
+    } else if (number >= 20 && number < 100) {
+        if (+backTwoDigits.toString()[1] === 0) {
+            return tens[backTwoDigits.toString()[0] - 2];
+        }
+        return `${tens[backTwoDigits.toString()[0] - 2]} ${
+            ones[number.toString()[1]]
+        }`;
+    } else {
+        if (+number.toString()[1] === 0) {
+            if (+number.toString()[2] === 0) {
+                return `${ones[number.toString()[0]]} hundred`;
+            }
+        }
+        return `${ones[number.toString()[0]]} hundred ${toReadable(
+            backTwoDigits
+        )}`;
+    }
+};
